9cfe07bb07cd378415f2a2a8e13fb10a4c229e22
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-04-30 21:50:30 -0700

    add clothes button added. code refactored slightly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,17 +11,21 @@ const avatar =
   "https://i.ytimg.com/vi/zbF3SsoTUvA/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC8cp5-bDhUMAEv7i6tBacv9Qp5VQ";
 // Add Clothes Button
 // Header return
-const Header = ({ addClothes }) => {
+const Header = ({ onAddClothes }) => {
   return (
     <header className="Header">
       <img className="Header__Logo" src={logo} alt="Logo" />
       <div className="Header__Date">{getCurrentDate()}</div>
       <div className="Header__Location">***See Section 5***</div>
-      <button className="Header__Add-Clothes-Btn" onClick={addClothes}>
+      <button className="Header__Add-Clothes-Btn" onClick={onAddClothes}>
         + Add Clothes
       </button>
       <p className="Header__Username">{userName}</p>
-      <img className="Header__Avatar" src={avatar} alt="avatar" />
+      <img
+        className="Header__Avatar"
+        src={avatar}
+        alt={`${userName}'s avatar`}
+      />
     </header>
   );
 };
