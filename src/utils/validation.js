8e65fab09e8aa6e0b7d51e1523d0265d82d30aa7
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-05-19 22:48:31 -0700

    validation.js and modalwithform error css added. skeleton layedout

---
+++ src/utils/validation.js
@@ -0,0 +1,31 @@
+const settings = {
+  formSelector: ".modal-with-form__form",
+  inputSelector: ".modal-with-form__input",
+  inputRadioSelector: ".modal-with-form__radio-input",
+  submitButtonSelector: ".modal-with-form__add-garment-btn",
+  inactiveButtonClass: ".modal-with-form__add-garment-btn_disabled",
+  inputErrorClass: ".modal-with-form__input_type_error",
+  errorClass: ".modal-with-form_error_visible",
+};
+
+const showInputError = (formElement, inputElement, errorMessage, config) => {};
+
+const hideInputError = () => {};
+
+const checkInputValidity = () => {};
+
+const hasInvalidInput = () => {};
+
+const toggleButtonState = () => {};
+
+const enableButton = () => {};
+
+const disableButton = () => {};
+
+const resetValidation = () => {};
+
+const setEventListeners = () => {};
+
+const enableValidation = () => {};
+
+export { enableValidation, resetValidation, disableButton, settings };
